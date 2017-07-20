8908a49b1542a324099773222c3b0e94e03be20c
danil <danil1996-96@mail.ru>
2017-07-20 22:42:09 +0300

    added sql init script

--- src/main/java/com/store/onlinestore/OnlineStoreApplication.java
+++ src/main/java/com/store/onlinestore/OnlineStoreApplication.java
@@ -2,6 +2,7 @@ package com.store.onlinestore;
 
 import com.store.onlinestore.model.entity.User;
 import com.store.onlinestore.model.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,13 +10,15 @@ import org.springframework.context.annotation.Bean;
 import org.h2.tools.Server;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.Arrays;
 
 @SpringBootApplication
 @Configuration
-@ImportResource({"./context.xml"})
+//@ImportResource({"./context.xml"})
 public class OnlineStoreApplication {
 
 	public static void main(String[] args) {
@@ -26,7 +29,8 @@ public class OnlineStoreApplication {
 			System.out.println(beanName);
 		}
 
-		UserRepository userRepository = (UserRepository) ctx.getBean("customerRepository");
+
+		UserRepository userRepository = (UserRepository) ctx.getBean("userRepository");
 		User user = userRepository.findByLogin("admin");
 		System.out.println(user.toString());
 	}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
+drop table users;
 create table users (id integer, login varchar2(30), password varchar2(30), role varchar2(30), version integer);
-insert into user values as (0, "admin", "admin", "admin", 1);
+insert into users values (0, 'admin', 'admin', 'admin', 1);
