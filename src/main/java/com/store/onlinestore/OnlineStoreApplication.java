0e9bcaafb76c8bda3ec1285a8363d97a669fd74d
danil <danil1996-96@mail.ru>
2017-07-22 14:21:57 +0300

    added dependencies for Log4j

--- src/main/java/com/store/onlinestore/OnlineStoreApplication.java
+++ src/main/java/com/store/onlinestore/OnlineStoreApplication.java
@@ -2,6 +2,8 @@ package com.store.onlinestore;
 
 import com.store.onlinestore.model.entity.User;
 import com.store.onlinestore.model.repositories.UserRepository;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,12 +12,17 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.h2.tools.Server;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.Arrays;
 
 @SpringBootApplication
 @Configuration
 public class OnlineStoreApplication extends SpringBootServletInitializer {
+	private static final Logger log = Logger.getLogger(OnlineStoreApplication.class);
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -33,7 +40,7 @@ public class OnlineStoreApplication extends SpringBootServletInitializer {
 
 		UserRepository userRepository = (UserRepository) ctx.getBean("userRepository");
 		User user = userRepository.findByLogin("admin");
-		System.out.println(user.toString());
+		log.info(user.toString());
 	}
 
 	@Bean(initMethod = "start", destroyMethod = "stop")
