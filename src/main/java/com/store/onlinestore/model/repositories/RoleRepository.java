518e5e154b1d232ac7997e6b7215c0fbdd901330
dach1016 <danil1996-96@mail.ru>
2017-07-25 17:59:45 +0300

    First elements of security functionality

---
+++ src/main/java/com/store/onlinestore/model/repositories/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.store.onlinestore.model.repositories;
+
+import com.store.onlinestore.model.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by dach1016 on 24.07.2017.
+ *
+ */
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+}
