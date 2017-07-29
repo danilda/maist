aa0154cbbaa850b883d35d114b1b48c4e5b36658
AntonCheliadin <chel_anton@mail.ru>
2017-07-29 22:18:00 +0300

    rename project to maist

--- src/main/java/ua/com/maist/MaistApplication.java
+++ src/main/java/ua/com/maist/MaistApplication.java
@@ -1,7 +1,7 @@
-package com.store.onlinestore;
+package ua.com.maist;
 
-import com.store.onlinestore.model.entity.User;
-import com.store.onlinestore.model.repositories.UserRepository;
+import ua.com.maist.model.entity.User;
+import ua.com.maist.model.repositories.UserRepository;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,20 +11,21 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.h2.tools.Server;
 import org.springframework.context.annotation.Configuration;
+
 import java.sql.SQLException;
 
 @SpringBootApplication
 @Configuration
-public class OnlineStoreApplication extends SpringBootServletInitializer {
-	private static final Logger log = Logger.getLogger(OnlineStoreApplication.class);
+public class MaistApplication extends SpringBootServletInitializer {
+	private static final Logger log = Logger.getLogger(MaistApplication.class);
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(OnlineStoreApplication.class);
+		return application.sources(MaistApplication.class);
 	}
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(OnlineStoreApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(MaistApplication.class, args);
 
 		UserRepository userRepository = (UserRepository) ctx.getBean("userRepository");
 		User user = userRepository.findByLogin("admin");
